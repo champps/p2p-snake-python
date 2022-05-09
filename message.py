44935a82f9cd924d619899fceda821b709750666
qzqz <gw9awgbu@anonaddy.me>
2022-05-09 18:21:48 +0300

    restructuring of clasess

---
+++ message.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+class message:
+    """
+    this class for transolate message
+    and find the currect command
+
+    """
+
+
+class compress:
+    """
+    for comress and decompress
+    algoretherms
+
+    """
+
+class command:
+    """
+    command after send reqiest
+
+    """
